47e2ebd937a1a2950a3172b7bb3687865ac7464f
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-01-11 23:49:10 +0700

    Fixing package

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // Membuka akses ke semua alamat IP
-    port: 5173, // Gunakan port default Vite (5173) atau sesuaikan
-  },
 });
